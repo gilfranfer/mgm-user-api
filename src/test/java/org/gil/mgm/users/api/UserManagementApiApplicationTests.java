9e7582360826b68690058120db66e54ecff9d13f
Francisco Gil <gil.franfer@gmail.com>
2025-02-11 15:45:41 -0600

    Resolves #7 - Initial Project setup

---
+++ src/test/java/org/gil/mgm/users/api/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gil.mgm.users.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
